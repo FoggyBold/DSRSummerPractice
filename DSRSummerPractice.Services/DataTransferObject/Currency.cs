ec5f93891c346cdb3d43b444f35d52d66ab394df
vitaliy <vitality.boldorev@yandex.ru>
2022-07-14 18:10:04 +0300

    add ICurrencyService, CurrencyService

---
+++ DSRSummerPractice.Services/DataTransferObject/Currency.cs
@@ -0,0 +1,11 @@
+﻿namespace DSR_Summer_Practice.Services.DataTransferObject
+{
+    /// <summary>
+    /// This class defines entitie of currency
+    /// </summary>
+    public class Currency
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
