50d7b7591d07e93360808d28cc35599065166e7a
vitaliy <vitality.boldorev@yandex.ru>
2022-07-16 15:28:09 +0300

    removed ExchangeRateViewModel and changed ExchangeRatesViewModel (added charCode, changed DateTime to string)

---
+++ DSRSummerPractice.WEB/ViewModels/ExchangeRatesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DSRSummerPractice.WEB.ViewModels;
+
+public class ExchangeRatesViewModel
+{
+    public string CurrencyName { get; set; }
+    //public IEnumerable<ExchangeRateViewModel> ExchangeRates { get; set; }
+    public IEnumerable<string> Days { get; set; }
+    public IEnumerable<double> Values { get; set; }
+}
